03b8db6f8739f71cbb2553ab9590346aa587e618
ditcugithub <qtai2556@gmail.com>
2024-09-07 17:55:21 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the path to Xcode command line tools
+export PATH="/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH"
+
+# Create a build directory
+mkdir -p build
+cd build
+
+# Compile the dynamic library
+clang -dynamiclib -o libfloatingbrowser.dylib ../src/floating_browser.m -framework Cocoa -framework WebKit
+
+echo "Build complete. The library is located in build/libfloatingbrowser.dylib"
